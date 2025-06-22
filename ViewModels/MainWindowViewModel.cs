1e9804933628449e266f6b4d2ff600ffe44e162e
maria <s62565@student.ubb.edu.pl>
2025-06-22 18:39:15 +0200

    Add project files.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Projekt_P4.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Projekt_P4.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
